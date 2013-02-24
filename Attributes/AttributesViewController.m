5b0dd23136cf56e0c94255315eadbf2760bc3054
David Hoelzer <dhoelzer@enclaveforensics.com>
2013-02-24 14:29:11 -0500

    Initial commit

--- Attributes/AttributesViewController.m
+++ Attributes/AttributesViewController.m
@@ -9,21 +9,75 @@
 #import "AttributesViewController.h"
 
 @interface AttributesViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *selectedWordLabel;
+@property (weak, nonatomic) IBOutlet UIStepper *selectedWordStepper;
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
 
 @end
 
 @implementation AttributesViewController
 
-- (void)viewDidLoad
+-(void)addLabelAttributes:(NSDictionary *)attributes range:(NSRange)range
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    if(range.location != NSNotFound)
+    {
+        NSMutableAttributedString *mAS = [self.messageLabel.attributedText mutableCopy];
+        [mAS addAttributes:attributes range:range];
+        self.messageLabel.attributedText = mAS;
+    }
+    
 }
 
-- (void)didReceiveMemoryWarning
+-(void)addSelectedWordAttributes:(NSDictionary *)attributes
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSRange range = [[self.messageLabel.attributedText string] rangeOfString:[self selectedWord]];
+    [self addLabelAttributes:attributes range:range];
 }
+
+- (IBAction)changeAttribute:(id)sender {
+    UIButton *selection = sender;
+    NSString *change = [[selection attributedTitleForState:UIControlStateNormal] string];
+
+    if ([change isEqualToString:@"Underline"]) {
+        [self addSelectedWordAttributes:@{NSUnderlineStyleAttributeName : @(NSUnderlineStyleSingle)} ];
+    }
+    if ([change isEqualToString:@"No Underline"])
+    {
+        [self addSelectedWordAttributes:@{NSUnderlineStyleAttributeName : @(NSUnderlineStyleNone)} ];
+    }
+}
+
+- (IBAction)changeSelectedWordColor:(UIButton *)sender {
+    [self addSelectedWordAttributes:@{ NSForegroundColorAttributeName : sender.backgroundColor}];
+}
+
+
+-(NSArray *)wordList
+{
+    NSArray *wordList = [[self.messageLabel.attributedText string] componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if([wordList count])
+    {
+        return wordList;
+    } else {
+        return @[@""];
+    }
+}
+
+-(NSString *)selectedWord
+{
+    return [self wordList][(int)self.selectedWordStepper.value];
+}
+
+- (IBAction)updateSelectedWord {
+    self.selectedWordStepper.maximumValue = [[self wordList] count] - 1;
+    self.selectedWordLabel.text = [self selectedWord];
+}
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self updateSelectedWord];
+}
+
 
 @end
