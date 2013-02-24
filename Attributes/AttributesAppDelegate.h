d433aa31895b25b745a8690539b59326dcb5c5e7
David Hoelzer <dhoelzer@enclaveforensics.com>
2013-02-23 21:20:44 -0500

    Initial Commit

---
+++ Attributes/AttributesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AttributesAppDelegate.h
+//  Attributes
+//
+//  Created by David Hoelzer on 2/23/13.
+//  Copyright (c) 2013 EnclaveForensics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttributesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Attributes/AttributesViewController.m
@@ -0,0 +1,29 @@
+//
+//  AttributesViewController.m
+//  Attributes
+//
+//  Created by David Hoelzer on 2/23/13.
+//  Copyright (c) 2013 EnclaveForensics. All rights reserved.
+//
+
+#import "AttributesViewController.h"
+
+@interface AttributesViewController ()
+
+@end
+
+@implementation AttributesViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
